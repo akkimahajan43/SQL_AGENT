dc3122040239d3822ae2546595078eade562478c
akshaym <akshaybmahajan95@gmail.com>
2026-05-12 18:17:22 +0530

    Added Database Code in app file

--- sql_agent/app.py
+++ sql_agent/app.py
@@ -4,6 +4,49 @@ from utils.llm import generate_sql
 from utils.db import run_query, get_schema
 from utils.validator import validate_query
 from utils.charts import create_chart
+#----------------------------------
+#created database
+#----------------------------------
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS sales (
+    id INTEGER PRIMARY KEY,
+    customer TEXT,
+    product TEXT,
+    amount REAL,
+    city TEXT,
+    order_date TEXT
+)
+""")
+
+data = [
+    ("Akshay", "Laptop", 80000, "Pune", "2026-05-01"),
+    ("Rahul", "Mouse", 500, "Mumbai", "2026-05-02"),
+    ("Sneha", "Keyboard", 2000, "Delhi", "2026-05-03"),
+    ("Akshay", "Monitor", 15000, "Pune", "2026-05-04"),
+]
+
+cursor.executemany("""
+INSERT INTO sales (
+    customer,
+    product,
+    amount,
+    city,
+    order_date
+)
+VALUES (?, ?, ?, ?, ?)
+""", data)
+
+conn.commit()
+
+conn.close()
+
+print("Database Created")
 # --------------------------------
 # Page Config
 # --------------------------------
